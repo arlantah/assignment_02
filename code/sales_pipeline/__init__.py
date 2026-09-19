cfe1e7e0c42d0bfb24e18b75630b6252744fd958
arlantah <zacharymoore057@gmail.com>
2026-09-19 03:32:05 +0000

    step 12

--- code/sales_pipeline/__init__.py
+++ code/sales_pipeline/__init__.py
@@ -69,8 +69,14 @@ HOW TO BUILD IT
 # package called display installed on this machine".
 
 from .extract import get_raw_sales_data
-from .transform import calculate_total_revenue, clean_sales_data
-from .display import print_sales_table
+from .transform import (
+    calculate_total_revenue,
+    clean_sales_data,
+    find_top_entry,
+    summarize_by_day,
+    summarize_by_item,
+)
+from .display import print_day_table, print_item_table, print_sales_table
 
 
 # `__all__` is a list of strings naming the public API. It does two jobs.
@@ -89,5 +95,10 @@ __all__ = [
     "get_raw_sales_data",
     "calculate_total_revenue",
     "clean_sales_data",
+    "summarize_by_item",
+    "summarize_by_day",
+    "find_top_entry",
+    "print_item_table",
+    "print_day_table",
     "print_sales_table",
 ]
